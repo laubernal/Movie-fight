36e9154bff834c562ebb741b33aed11daf2d2f8b
laubernal <lbernalrodrguez@gmail.com>
2021-06-13 13:02:28 +0200

    dropdown features

--- index.js
+++ index.js
@@ -23,22 +23,54 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 };
 
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+  <label><b>Search</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
+
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
+ 
+  if (!movies.length) {
+    dropdown.classList.remove('is-active');
+    return;
+  }
 
+  resultsWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
   for (let movie of movies) {
-    const div = document.createElement("div");
-    // img === 'N/A' ? '' : movie.Poster;
+    const option = document.createElement("a");
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
 
-    div.innerHTML = `
-      <img src="${movie.Poster}"; />
-      <h1>${movie.Title} ${movie.Year}</h1>
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+      <img src='${imgSrc}'; />
+      ${movie.Title} (${movie.Year})
     `;
 
-    document.querySelector('#target').appendChild(div);
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      input.value = movie.Title;
+    });
+
+    resultsWrapper.appendChild(option);
   }
 };
 
 input.addEventListener("input", debounce(onInput, 500));
+
+document.addEventListener("click", (event) => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
